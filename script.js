d55d657c299973e9cf4367a53426cd243802ce60
Ash-Sable <jesuniranemmanuel@gmail.com>
2025-01-27 09:00:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add interactivity if needed
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Thank you for your message! I will get back to you soon.');
+});
